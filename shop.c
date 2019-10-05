d01cbc808b77b4cfd29b7a1797229eebbadec668
andkoc001 <and.koc001@gmail.com>
2019-10-05 19:23:19 +0100

    File created and set up.

---
+++ shop.c
@@ -0,0 +1,11 @@
+// Shop simulator
+// Author: Andrzej Kocielski
+// Created: 05-10-2019
+// Multi-Paredigm Programming, GMIT 2019
+// Lecturer: Dominic Carr
+
+int main(void)
+{
+  printf("Hello World\n");
+  return 0;
+}
